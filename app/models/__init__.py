1f54777acedecdd595632d618b7d71303303d264
PersonHow <SecreTary_RC@hotmail.com>
2026-04-14 16:46:23 +0800

    add four model .py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,9 @@
 from app.models.chunk import Chunk
 from app.models.document import Document
 from app.models.session import IngestionSession
-# from app.models.company_rule import CompanyRule
-# from app.models.company import Company
-# from app.models.product import Product
-# from app.models.user import User
-# __all__ = ["IngestionSession", "Document", "Chunk", "CompanyRule", "Company", "Product", "User"]
-__all__ = ["IngestionSession", "Document", "Chunk"]
+from app.models.company_rule import CompanyRule
+from app.models.company import Company
+from app.models.product import Product
+from app.models.user import User
+__all__ = ["IngestionSession", "Document", "Chunk", "CompanyRule", "Company", "Product", "User"]
+
